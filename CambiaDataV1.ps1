aed56919de571e43901860481c44eab9a3f80164
Alessio <76960572+VoidTechAddict@users.noreply.github.com>
2023-01-23 11:51:40 +0100

    Add files via upload

---
+++ CambiaDataV1.ps1
@@ -0,0 +1,31 @@
+﻿# Richiesta 
+$Title = "Vuoi cambiare data?"
+$Prompt = "Scegli se ad un file o ad una cartella"
+$Choices = [System.Management.Automation.Host.ChoiceDescription[]] @("&File", "&Cartella", "&Esci")
+$Default = 1
+
+# scelta
+$Choice = $host.UI.PromptForChoice($Title, $Prompt, $Choices, $Default)
+
+# Azioni
+switch($Choice)
+{
+    0 {
+$namef = read-host "Inserisci il file"  
+$datew = read-host "Data scrittura GG/MM/AA HH:MM:SS"
+$datec = read-host "Data creazione GG/MM/AA HH:MM:SS"
+$file = Get-Item $namef
+$file.LastWriteTime = ($datew)
+$file.CreationTime = ($datec)}
+
+    1 {
+$namec = read-host "Inserisci cartella"
+$datew = read-host "Data scrittura GG/MM/AA HH:MM:SS"
+$datec = read-host "Data creazione GG/MM/AA HH:MM:SS"
+$folder = Get-Item $namec
+$file.LastWriteTime = ($datew)
+$folder.CreationTime = ($datec)}
+    2 { Write-Host "Exit"}
+}
+
+
